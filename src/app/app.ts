6d42e9a0191f16a3eb5d0d5c84072fdc3ab265fc
Facundo Bernardini <facubernardini@hotmail.com>
2026-07-09 22:30:34 -0300

    agregue google analytics

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,6 @@
-import { Component, signal } from '@angular/core';
+import { Component, DOCUMENT, inject, OnInit, Renderer2, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { environment } from 'src/environments/environment.dev';
 
 @Component({
   selector: 'app-root',
@@ -7,6 +8,34 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
+export class App implements OnInit {
   protected readonly title = signal('proyecto-catalogo');
+
+  private renderer = inject(Renderer2);
+  private document = inject(DOCUMENT);
+
+  ngOnInit(): void {
+    if (environment.production) {
+      this.inyectarScriptAnalytics();
+    }
+  }
+
+  private inyectarScriptAnalytics(): void {
+    const trackingId = environment.analyticsId; 
+
+    const scriptLibreria = this.renderer.createElement('script');
+    this.renderer.setAttribute(scriptLibreria, 'async', 'true');
+    this.renderer.setAttribute(scriptLibreria, 'src', `https://www.googletagmanager.com/gtag/js?id=${trackingId}`);
+    this.renderer.appendChild(this.document.head, scriptLibreria);
+
+    const scriptConfig = this.renderer.createElement('script');
+    const codigoConfig = this.renderer.createText(`
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', '${trackingId}');
+    `);
+    this.renderer.appendChild(scriptConfig, codigoConfig);
+    this.renderer.appendChild(this.document.head, scriptConfig);
+  }
 }
--- src/environments/environment.staging.ts
+++ src/environments/environment.staging.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  apiUrl: 'https://proyectocatalogo-api.onrender.com/api/v1'
+  production: true,
+  apiUrl: 'https://proyectocatalogo-api.onrender.com/api/v1',
+  analyticsId: 'G-90WN5EWHZ7'
 };
